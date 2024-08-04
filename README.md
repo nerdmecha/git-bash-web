e8c1b07692d806a1ce884e3c5cce6dd5f50bc007
Jonghun Kim <corner.jhkim@gmail.com>
2024-08-04 11:06:07 +0900

    Edited Names & Deleted Images

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### git-bash-web
 <strong>Web Development Started</strong>
 <p>Used Tools Visual Studio Code, Sourcetree, Git Bash</p>
-<p>Used Programming Languages HTML, CSS, Javascript, Jquery</p>
+<p>Used Programming Languages HTML, CSS, Javascript</p>
 <p>Other Programming Languages will be Added Later</p>
 <br />
 <strong>I'm back GitHub!</strong>
--- js/main.js
+++ js/main.js
@@ -1,31 +1,5 @@
-$(document).ready(function() {
-    var times = new Date();
-    var years = times.getFullYear();
-    var months = times.getMonth(); // 0 to 11
-    var days = times.getDate();
-    var hours = times.getHours();
-    var hoursCalc;
-    var hoursResult;
-    var mins = times.getMinutes();
-    var state;
+function basic(a) {
+    return a;
+};
 
-    const monthsArray = [
-        "Jan", "Feb", "Mar", "Apr",
-        "May", "Jun", "Jul", "Aug",
-        "Sep", "Oct", "Nov", "Dec" 
-    ]
-
-    var monthsString = monthsArray[months]; // if 0 equals JAN
-
-    if (hours < 12) {
-        state = "AM";
-        hoursCalc = hours;
-        hoursResult = String(hoursCalc).padStart(2, "0");
-    } if (hours >= 12) {
-        state = "PM";
-        hoursCalc = hours - 12;
-        hoursResult = String(hoursCalc).padStart(2, "0");
-    }
-    alert("Hello Again" + " " + years + " " + monthsString + " " + days + " " + state + " " + hoursResult + ":" + mins + "\n"
-        + "Lastest Updated at 2024-02-28 PM 03:15 UTC+09:00");
-})
+alert(basic(5));
