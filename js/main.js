5c62ec7aeac83e60f2d3f49dd2335f6fe393d9b4
Jonghun Kim <corner.jhkim@gmail.com>
2024-08-09 20:29:38 +0900

    Added Submenu CSS & JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-    // added soon...
+const divTags = document.querySelectorAll(".obj > div");
+
+divTags.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        const subdiv = div.querySelector(".subdiv");
+        subdiv.style.display = "block";
+    })
+
+    div.addEventListener("mouseout", () => {
+        const subdiv = div.querySelector(".subdiv");
+        subdiv.style.display = "none";
+    })
 })
